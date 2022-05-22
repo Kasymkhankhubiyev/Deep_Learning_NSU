1b60438f717c77872c16e1dbd34fb330c70bf30a
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2022-05-22 10:40:45 +0700

    Add files via upload

---
+++ assignment2/layers.py
@@ -0,0 +1,173 @@
+import numpy as np
+
+
+def l2_regularization(W, reg_strength):
+    """
+    Computes L2 regularization loss on weights and its gradient
+
+    Arguments:
+      W, np array - weights
+      reg_strength - float value
+
+    Returns:
+      loss, single value - l2 regularization loss
+      gradient, np.array same shape as W - gradient of weight by l2 loss
+    """
+    # TODO: Copy from the previous assignment
+    loss = reg_strength * np.trace(np.matmul(W.T, W))  # L2(W) = λ * tr(W.T * W)
+    grad = 2 * reg_strength * W  # dL2(W)/dW = 2 * λ * W
+    return loss, grad  # L2(W), dL2(W)/dW
+
+def softmax(predictions):
+    """
+    Computes probabilities from scores
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) - classifier output
+    Returns:
+      probs, np array of the same shape as predictions - probability for every class, 0..1
+    """
+
+    maximums = np.amax(predictions, axis=1).reshape(predictions.shape[0], 1)
+    predictions_ts = predictions - maximums
+
+    predictions_exp = np.exp(predictions_ts)
+    sums = np.sum(predictions_exp, axis=1).reshape(predictions_exp.shape[0], 1)
+    result = predictions_exp / sums
+
+    return result   # S
+
+
+def cross_entropy_loss(probs, target_index):
+    """
+    Computes cross-entropy loss
+    Arguments:
+      probs, np array, shape is either (N) or (batch_size, N) - probabilities for every class
+      target_index: np array of int, shape is (1) or (batch_size) - index of the true class for given sample(s)
+    Returns:
+      loss: single value
+    """
+
+    rows = np.arange(target_index.shape[0])
+    cols = target_index
+
+    return np.mean(-np.log(probs[rows, cols]))  # L
+
+def softmax_with_cross_entropy(predictions, target_index):
+    """
+    Computes softmax and cross-entropy loss for model predictions,
+    including the gradient
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+
+    Returns:
+      loss, single value - cross-entropy loss
+      dprediction, np array same shape as predictions - gradient of predictions by loss value
+    """
+    # TODO: Copy from the previous assignment
+    probs = softmax(predictions)  # S
+    loss = cross_entropy_loss(probs, target_index)  # L
+
+    indicator = np.zeros(probs.shape)
+    indicator[np.arange(probs.shape[0]), target_index] = 1  # 1(y)
+    dprediction = (probs - indicator) / predictions.shape[0]  # dL/dZ = (S - 1(y)) / N
+
+    return loss, dprediction
+
+
+class Param:
+    """
+    Trainable parameter of the model
+    Captures both parameter value and the gradient
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.grad = np.zeros_like(value)
+
+    def reset_grad(self):
+        self.grad = np.zeros_like(self.value)
+
+
+class ReLULayer:
+    def __init__(self):
+        self.cache = None
+
+    def forward(self, X):
+        # TODO: Implement forward pass
+        # Hint: you'll need to save some information about X
+        # to use it later in the backward pass
+        self.cache = np.greater(X, 0).astype(float)  # dZ/dx
+        out = np.maximum(X, 0)  # Z
+        return out
+
+    def backward(self, d_out):
+        """
+        Backward pass
+
+        Arguments:
+        d_out, np array (batch_size, num_features) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result: np array (batch_size, num_features) - gradient
+          with respect to input
+        """
+        # TODO: Implement backward pass
+        # Your final implementation shouldn't have any loops
+        d_result = np.multiply(d_out, self.cache)  # dL/dX = dL/dZ * dZ/dX
+        return d_result
+
+    def params(self):
+        # ReLU Doesn't have any parameters
+        return {}
+
+
+class FullyConnectedLayer:
+    def __init__(self, n_input, n_output):
+        self.W = Param(0.001 * np.random.randn(n_input, n_output))
+        self.B = Param(0.001 * np.random.randn(1, n_output))
+        self.X = None
+
+    def forward(self, X):
+        # TODO: Implement forward pass
+        # Your final implementation shouldn't have any loops
+        self.X = X
+        return np.dot(X, self.W.value) + self.B.value
+
+    def backward(self, d_out):
+        """
+        Backward pass
+        Computes gradient with respect to input and
+        accumulates gradients within self.W and self.B
+
+        Arguments:
+        d_out, np array (batch_size, n_output) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result: np array (batch_size, n_input) - gradient
+          with respect to input
+        """
+        # TODO: Implement backward pass
+        # Compute both gradient with respect to input
+        # and gradients with respect to W and B
+        # Add gradients of W and B to their `grad` attribute
+
+        # It should be pretty similar to linear classifier from
+        # the previous assignment
+
+        d_result = np.dot(d_out, self.W.value.T)  # dL/dX = dL/dZ * W.T
+        dLdW = np.dot(self.X.T, d_out)  # dL/dW = X.T * dL/dZ
+        dLdB = 2 * np.mean(d_out, axis = 0)  # dL/dW = dL/dZ * dZ/dB = I * dL/dZ
+
+        self.W.grad += dLdW
+        self.B.grad += dLdB
+
+        return d_result
+
+    def params(self):
+        return {'W': self.W, 'B': self.B}
---
+++ assignment2/model.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+from layers import FullyConnectedLayer, ReLULayer, softmax_with_cross_entropy, softmax, l2_regularization
+
+
+class TwoLayerNet:
+    """ Neural network with two fully connected layers """
+
+    def __init__(self, n_input, n_output, hidden_layer_size, reg):
+        """
+        Initializes the neural network
+
+        Arguments:
+        n_input, int - dimension of the model input
+        n_output, int - number of classes to predict
+        hidden_layer_size, int - number of neurons in the hidden layer
+        reg, float - L2 regularization strength
+        """
+        self.reg = reg
+
+        self.layers = [
+            FullyConnectedLayer(n_input, hidden_layer_size),
+            ReLULayer(),
+            FullyConnectedLayer(hidden_layer_size, n_output)
+        ]
+
+    def compute_loss_and_gradients(self, X, y):
+        """
+        Computes total loss and updates parameter gradients
+        on a batch of training examples
+
+        Arguments:
+        X, np array (batch_size, input_features) - input data
+        y, np array of int (batch_size) - classes
+        """
+
+        assert X.ndim == 2
+        assert y.ndim == 1
+        assert X.shape[0] == y.shape[0]
+
+        for _, param in self.params().items():
+            param.reset_grad()
+        # Hint: using self.params() might be useful!
+
+        # forward pass
+        out = X
+        for layer in self.layers:
+            out = layer.forward(out)
+
+        # backward pass
+        loss, d_out = softmax_with_cross_entropy(out, y)
+        for layer in reversed(self.layers):
+            d_out = layer.backward(d_out)
+
+        # l2 regularization
+        for _, param in self.params().items():
+            reg_loss, reg_grad = l2_regularization(param.value, self.reg)
+            loss += reg_loss
+            param.grad += reg_grad
+
+        return loss
+
+    def predict(self, X):
+        """
+        Produces classifier predictions on the set
+
+        Arguments:
+          X, np array (test_samples, num_features)
+
+        Returns:
+          y_pred, np.array of int (test_samples)
+        """
+        out = X
+        for layer in self.layers:
+            out = layer.forward(out)
+        out = softmax(out)
+
+        pred = np.argmax(out, axis = 1)
+        return pred
+
+    def params(self):
+        result = {}
+
+        # TODO Implement aggregating all of the params
+        for index, layer in enumerate(self.layers):
+            for name, param in layer.params().items():
+                result['%s_%s' % (index, name)] = param
+
+        return result
